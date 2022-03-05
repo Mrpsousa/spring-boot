1f60117d929b2af66ecabe64096dbbe6a5b6e034
Mrpsousa <urameshi.uba@gmail.com>
2022-03-04 23:17:42 -0300

    feature: produto e auxiliares

--- projeto-01/src/main/java/com/mrpsousa/Projeto01Application.java
+++ projeto-01/src/main/java/com/mrpsousa/Projeto01Application.java
@@ -1,13 +1,47 @@
 package com.mrpsousa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projeto01Application {
+import com.mrpsousa.domain.Categoria;
+import com.mrpsousa.domain.Produto;
+import com.mrpsousa.repositories.CategoriaRepository;
+import com.mrpsousa.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class Projeto01Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora",8000.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2 ,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
